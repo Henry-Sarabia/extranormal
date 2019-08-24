d09a762227f32c2e0b08c3cd2b40cc489eb19923
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-08-23 18:00:30 -0700

    Add CreatedAt field to Item struct

--- item.go
+++ item.go
@@ -1,11 +1,14 @@
 package extranormal
 
+import "time"
+
 type Item struct {
-	Name        string  `json:"name"`
-	Class       string  `json:"class"`
-	Value       float64 `json:"value"`
-	Weight      float64 `json:"weight"`
-	Description string  `json:"description"`
+	Name        string    `json:"name"`
+	Class       string    `json:"class"`
+	Value       float64   `json:"value"`
+	Weight      float64   `json:"weight"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 type ItemService interface {
